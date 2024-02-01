0bde0489215e1d5da414ff13847e72187de829ff
EnesSiracDev <esiractopaloglu.dev@gmail.com>
2024-02-01 19:29:48 +0300

    Add project files.

---
+++ Basic_Calculator_Form/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic_Calculator_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Addition");
+            comboBox1.Items.Add("Subtraction");
+            comboBox1.Items.Add("Multiplication");
+            comboBox1.Items.Add("Division");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Operation operation = new Operation(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+            switch (comboBox1.Text)
+            {
+                case "Addition":
+                    MessageBox.Show(operation.Addition().ToString());
+                    break;
+                case "Subtraction":
+                    MessageBox.Show(operation.Subtraction().ToString());
+                    break;
+                case "Multiplication":
+                    MessageBox.Show(operation.Multiplication().ToString());
+                    break;
+                case "Division":
+                    MessageBox.Show(operation.Division().ToString());
+                    break;
+            }
+        }
+    }
+}
---
+++ Basic_Calculator_Form/Operation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Calculator_Form
+{
+    internal class Operation
+    {
+        int sayi1, sayi2;
+
+        public Operation(int s1, int s2)
+        {
+            sayi1 = s1;
+            sayi2 = s2;
+        }
+
+        public int Addition()
+        {
+            return sayi1 + sayi2;
+        }
+
+        public int Subtraction()
+        {
+            return sayi1 - sayi2;
+        }
+
+        public int Multiplication()
+        {
+            return sayi1 * sayi2;
+        }
+
+        public int Division()
+        {
+            return sayi1 / sayi2;
+        }
+    }
+}
